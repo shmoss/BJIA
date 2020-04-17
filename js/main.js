ca1b12f969deef030bf121bbfaf9e45ac34ac15a
shmoss <shmoss@wisc.edu>
2020-04-17 11:49:47 -0500

    work on programs page
    left align vertical nav for programs

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 $('.carousel').carousel({
   interval: 2000
 })
+
+$(document).ready(function(){ 
+  $('.tileHeading').on('click', function(e) {
+    // Prevent the page from reloading
+  
+
+    // Remove .active from the current active .tileHeading
+    $('.tileHeading.active').removeClass('active');
+
+    // Set the .active class to the current clicked element
+    $(this).addClass('active');  
+
+  });
+});
